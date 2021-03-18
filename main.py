4388f47f6d8fe04f5aaa4a5a1a9438284bf75af3
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-03-19 00:25:46 +0700

    implement: GetEventsByDate

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ import hts.participant.service_pb2_grpc as participant_service_grpc
 
 from db_model import Feedback, Event, EventDuration, UserEvent, session, Tag, EventTag, UserEventFeedback
 from helper import getInt64Value, b64encode
+from datetime import datetime
 from sqlalchemy import func
-from google.protobuf.timestamp_pb2 import Timestamp
 import random
-import pytz
 
 
 class ParticipantService(participant_service_grpc.ParticipantServiceServicer):
@@ -167,6 +166,39 @@ class ParticipantService(participant_service_grpc.ParticipantServiceServicer):
             return participant_service.EventsResponse(event=tag_events)
         return participant_service.EventsResponse()
 
+    def GetEventsByDate(self, request, context):
+        try:
+            text = float(request.text)
+        except:
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("Wrong timestamp format.")
+            return proto_pb2.Response()
+
+        date = datetime.fromtimestamp(text)
+        start_date = datetime(date.year, date.month, date.day, 0, 0, 0)
+        end_date = datetime(date.year, date.month, date.day + 1, 0, 0, 0)
+
+        events = session.query(EventDuration).filter(
+            EventDuration.start >= start_date, EventDuration.start < end_date).all()
+
+        events_id = []
+        date_events = []
+
+        for event in events:
+            events_id.append(event.id)
+
+        for event_id in events_id:
+            event = session.query(Event).filter(
+                Event.id == event_id).scalar()
+            if (event is not None):
+                date_events.append(common.Event(id=event.id, organization_id=event.organization_id, event_location_id=None, description=event.description, name=event.name,
+                                                cover_image=event.cover_image, cover_image_hash=event.cover_image_hash, poster_image=event.poster_image, poster_image_hash=event.poster_image_hash, contact=event.contact))
+
+        if (date_events):
+            return participant_service.EventsResponse(event=date_events)
+        return participant_service.EventsResponse()
+
+    def GetEventsByFacilityString(self, request, context):
     def GenerateQR(self, request, context):
         user_event = {"id": request.id, "user_id": request.user_id,
                       "event_id": request.event_id}
